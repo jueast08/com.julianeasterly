1e41eb89bf005c432932fb32acf307a0d245b411
Julian Easterly <julianeasterly@gmail.com>
2020-04-26 20:53:19 +0200

    header initialisation

---
+++ src/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="header">
+    <div class="header__logo">
+      <MainLogo></MainLogo>
+    </div>
+  </div>
+</template>
+
+<script>
+import MainLogo from 'Icons/MainLogo';
+
+export default {
+  name: 'Header',
+  props: {
+    msg: String
+  },
+  components: {
+    MainLogo
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+@use 'global';
+
+  #header {
+    //background: black;
+    width: 100%; //xl
+    
+    &__logo {
+      background: bisque;
+    }    
+  }
+
+@include global.adapt-to-screen('xl') {
+  #header {
+    width: 100%; //xl
+    
+    &__logo {
+      background: bisque;
+    }    
+  }
+}
+</style>
