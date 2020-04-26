2e158236032a6b4b37224c47405bcead1c1cb117
Julian Easterly <julianeasterly@gmail.com>
2020-04-26 21:00:08 +0200

    scss for body of the site

--- src/App.vue
+++ src/App.vue
@@ -14,4 +14,16 @@ export default {
 </script>
 
 <style lang="scss">
+  body {
+    height: 100vh;
+    width: 100vw;
+    padding: 0;
+    margin: 0;
+
+    #app {
+      height: 100%;
+      width: 100%;
+    }
+  }
+
 </style>
