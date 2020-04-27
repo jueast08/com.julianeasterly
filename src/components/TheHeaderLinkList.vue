a829d8c2b8696b74f9632a4f141d3f527be016ae
Julian Easterly <julianeasterly@gmail.com>
2020-04-27 17:48:22 +0200

    Refactored the header ___ Note : for TheHeaderLinkList, I used the slot method. Not sure if this is the best choice but I think it will allow some flexibility when I start doing the internationalisaiton of the web app

---
+++ src/components/TheHeaderLinkList.vue
@@ -0,0 +1,40 @@
+<template>
+  
+  <div class="the-header-link-list">
+    <slot></slot>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'TheHeaderLogo',
+}
+</script>
+
+<style lang="scss" scoped>
+  @use 'global';
+
+  .the-header-link-list {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  span {
+    margin: 0 10px;
+    font-family: 'Poppins', sans-serif;
+    font-size: 14px;
+    font-weight: 700;
+    color: global.$primary-white;
+    
+    &:hover {
+      cursor: pointer;
+    }
+    
+    &.active {
+      font-size: 20px;
+    }
+  }
+}
+</style>
