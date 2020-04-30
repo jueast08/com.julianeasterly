4eb896c32bcb48b90b3e6a940c677eef82c2e15c
Julian Easterly <julianeasterly@gmail.com>
2020-04-30 12:24:28 +0200

    updated animations and refactored code

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
     chainWebpack: config => {
         let alias = config.resolve.alias;
 
-        alias.set('Root', path.resolve(__dirname,''));
+        alias.set('Source', path.resolve(__dirname,'src'));
         alias.set('Components', path.resolve(__dirname, 'src/components'));
         alias.set('Icons', path.resolve(__dirname, 'src/components/icons'));
         alias.set('Assets', path.resolve(__dirname, 'src/assets')); 
