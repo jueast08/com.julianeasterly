abcc75e0245ddfd77d4210e05aed3bd19ad1ee50
Julian Easterly <julianeasterly@gmail.com>
2020-04-30 17:54:44 +0200

    initialized and AboutSection

---
+++ src/components/TheAboutSection.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="about col-12">
+    <div class="about__container col-12">
+
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: 'TheAboutSection',
+
+}
+</script>
+
+<style lang="scss" scoped>
+  @use 'global';
+  
+  .about {
+    height: 100vh;
+    
+    &__container {
+      height: 100%;
+      background: global.$background-gray;
+    }
+  }
+ 
+</style>
