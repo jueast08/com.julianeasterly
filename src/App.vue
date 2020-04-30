9e3ff87674b02582b1a69cff677e2d43d16e2097
Julian Easterly <julianeasterly@gmail.com>
2020-04-30 17:54:19 +0200

    Added a button that will send the user to the top of the page (js to be coded

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@
     <div id="top-anchor-pixel" style="position: absolute; top:70px;"></div>
     <the-header/>
     <the-social-network-bar/>
+    <the-go-to-top-button/>
     <the-home-section/>
-    <!-- TODO delete -->
-    <div style="width: 100%; height: 100000px"></div>
+    <the-about-section/>
   </div>
 </template>
 
@@ -13,11 +13,14 @@
 import TheHeader from './components/TheHeader';
 import TheSocialNetworkBar from './components/TheSocialNetworkBar';
 import TheHomeSection from './components/TheHomeSection';
+import TheAboutSection from './components/TheAboutSection';
+import TheGoToTopButton from './components/TheGoToTopButton';
+
 
 
 export default {
   name: 'App',
-  components: {TheHeader, TheSocialNetworkBar, TheHomeSection}
+  components: {TheHeader, TheSocialNetworkBar, TheGoToTopButton, TheHomeSection, TheAboutSection}
 }
 </script>
 
---
+++ src/components/TheGoToTopButton.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="go-to-top">
+    <div class="go-to-top__arrow"></div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'TheGoToTopButton',
+  //@TODO code the scroll up action
+}
+</script>
+
+<style lang="scss" scoped>
+  @use 'global';
+  
+  .go-to-top {
+    position: fixed;
+    bottom: 30px;
+    right: 30px;
+    z-index: 100;
+    height: 50px;
+    width: 50px;
+    display: flex;
+    justify-content: center;
+    border-radius: 100%;
+    background: global.$primary-white;
+    -webkit-box-shadow: -10px 0px 20px 2px rgba(0,0,0,0.2);
+    -moz-box-shadow: -10px 0px 20px 2px rgba(0,0,0,0.2);
+    box-shadow: -10px 0px 20px 2px rgba(0,0,0,0.2);
+    cursor: pointer;
+    transition: (background ease-in-out 0.25s), (bottom ease-in-out 0.25s);
+
+    &__arrow {
+      height: 10px;
+      width: 10px;
+      transform-origin: center;
+      transform: translateY(20px) rotate(45deg);
+      border-color: global.$primary-black;
+      border-style: solid;
+      border-width: 3px 0 0 3px;
+    }
+
+    &:hover {
+      background: global.$primary-color;
+      bottom: 32px;
+    }
+    &:hover & {
+      &__arrow {
+        border-color: global.$primary-white;
+      }
+    }
+  }
+
+  @include global.adapt-to-screen('l') {
+    .go-to-top {
+      right: 84px;
+    }
+  }
+ 
+</style>
