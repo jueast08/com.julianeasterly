70ec0104b023e859b6179a64354dfabc85777259
Julian Easterly <julianeasterly@gmail.com>
2020-05-05 19:13:17 +0200

    added a contact me button to the skills section

--- src/components/TheSkillsSection.vue
+++ src/components/TheSkillsSection.vue
@@ -1,39 +1,45 @@
 <template>
   <base-section id="skills" title="What I Offer" subtitle="My Skills">
-      <div class="skills">
-      <base-content-icon class="skills__skill">
-          <template v-slot:icon><base-logo/></template>
-          <template v-slot:title>Development and Tech</template>
-          <template v-slot:description>
-            I have educational with Java, C and C++, as
-            well as professional experience in Git/Git-flow
-            Python, PHP, and Javascript.
-            I've worked with various tools such as Travis
-            CI, Sonarqube, Docker and Kubernetes.
-          </template>
-      </base-content-icon>
-      <base-content-icon class="skills__skill">
-          <template v-slot:icon><base-logo/></template>
-          <template v-slot:title>Project and Product Management</template>
-          <template v-slot:description>
-            I have educational with Java, C and C++, as
-            well as professional experience in Git/Git-flow
-            Python, PHP, and Javascript.
-            I've worked with various tools such as Travis
-            CI, Sonarqube, Docker and Kubernetes.
-          </template>          
-      </base-content-icon>
-      <base-content-icon class="skills__skill">
-          <template v-slot:icon><base-logo/></template>
-          <template v-slot:title>Do-It-Yourself</template>
-          <template v-slot:description>
-            I have educational with Java, C and C++, as
-            well as professional experience in Git/Git-flow
-            Python, PHP, and Javascript.
-            I've worked with various tools such as Travis
-            CI, Sonarqube, Docker and Kubernetes.
-          </template>   
-      </base-content-icon>
+    <div class="skills">
+      <div class="skills__list">
+        <base-content-icon class="skills__list__skill">
+            <template v-slot:icon><base-logo/></template>
+            <template v-slot:title>Development and Tech</template>
+            <template v-slot:description>
+              I have educational with Java, C and C++, as
+              well as professional experience in Git/Git-flow
+              Python, PHP, and Javascript.
+              I've worked with various tools such as Travis
+              CI, Sonarqube, Docker and Kubernetes.
+            </template>
+        </base-content-icon>
+        <base-content-icon class="skills__list__skill">
+            <template v-slot:icon><base-logo/></template>
+            <template v-slot:title>Project and Product Management</template>
+            <template v-slot:description>
+              I have educational with Java, C and C++, as
+              well as professional experience in Git/Git-flow
+              Python, PHP, and Javascript.
+              I've worked with various tools such as Travis
+              CI, Sonarqube, Docker and Kubernetes.
+            </template>          
+        </base-content-icon>
+        <base-content-icon class="skills__list__skill">
+            <template v-slot:icon><base-logo/></template>
+            <template v-slot:title>Do-It-Yourself</template>
+            <template v-slot:description>
+              I have educational with Java, C and C++, as
+              well as professional experience in Git/Git-flow
+              Python, PHP, and Javascript.
+              I've worked with various tools such as Travis
+              CI, Sonarqube, Docker and Kubernetes.
+            </template>   
+        </base-content-icon>
+      </div>
+      <div class="skills__contact-me">
+        <div>Want to work together on a project or opportunity?</div>
+        <div class="skills__contact-me__button"><base-round-button>contact me</base-round-button></div>
+      </div>
     </div>
   </base-section>
 </template>
@@ -42,11 +48,11 @@
 import BaseSection from 'Components/BaseSection';
 import BaseContentIcon from 'Components/BaseContentIcon';
 import BaseLogo from 'Icons/BaseLogo';
+import BaseRoundButton from 'Components/BaseRoundButton';
 
 export default {
   name: 'TheSkillsSection',
-  components: {BaseSection, BaseContentIcon,BaseLogo,},
-
+  components: {BaseSection, BaseContentIcon,BaseLogo, BaseRoundButton},
 }
 </script>
 
@@ -60,23 +66,40 @@ export default {
     }
     .skills {
       margin-top: 40px;
-      &__skill{
-        margin-top: 60px;
-        ::v-deep .base-content-icon {
-          &__icon {
-            fill: global.$primary-white;
+      
+      &__list {
+        
+        &__skill{
+          margin-top: 60px;
+          ::v-deep .base-content-icon {
+            &__icon {
+              fill: global.$primary-white;
+            }
+            &__title {
+              @include global.h4-font;
+              color: global.$primary-white;
+            }
+            &__description {
+              @include global.p-font;
+              color: global.$primary-white;
+            }  
           }
-          &__title {
-            @include global.h4-font;
-            color: global.$primary-white;
-          }
-          &__description {
-            @include global.p-font;
+        }
+      }
+
+      &__contact-me {
+        margin-top: 50px;
+        text-align: center;
+        @include global.subtitle-font;
+        color: global.$primary-white;
+
+        &__button{
+          margin-top: 25px;
+          button {
             color: global.$primary-white;
           }  
         }
       }
-    
     }
   }
  
