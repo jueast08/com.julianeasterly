11621f35c8eeb345070e479b78acb607ad497b46
Julian Easterly <julianeasterly@gmail.com>
2020-05-06 10:42:23 +0200

    Created a HobbyContentIcon based on the BaseContentIcon

---
+++ src/components/HobbyContentIcon.vue
@@ -0,0 +1,39 @@
+<template>
+  <base-content-icon>
+    <template v-slot:icon><slot name="icon"/></template>
+    <template v-slot:title><slot name="title"/></template>
+    <template v-slot:description><slot name="description"/></template>
+  </base-content-icon>
+</template>
+
+<script>
+import BaseContentIcon from 'Components/BaseContentIcon';
+
+export default {
+  name: 'HobbyContentIcon',
+  components: {BaseContentIcon,}
+}
+</script>
+
+
+<style lang="scss" scoped>
+  @use 'global';
+
+  ::v-deep .base-content-icon {
+    
+    &__icon {
+      svg, img {
+        fill: global.$primary-color;
+      }
+    }
+
+    &__title {
+      @include global.h4-font($weight: 300);
+      color: global.$primary-black;
+    }
+    
+    &__description {
+      display: none;
+    }
+  }
+</style>
