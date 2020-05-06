c280e44a83d2908fd078aab914d2d810062f081a
Julian Easterly <julianeasterly@gmail.com>
2020-05-06 17:19:01 +0200

    added an onsubmit button

--- src/components/TheContactSection.vue
+++ src/components/TheContactSection.vue
@@ -1,27 +1,39 @@
 <template>
   <base-section id="contact" title="Send Me a Message" subtitle="Opportunity, idea for a project, or just want to say hi?">
     <div class="contact">
-      <div class="contact__form">
+      <form class="contact__form" @submit.prevent="onSubmit"> <!-- @TODO change to a verification functionto be changed -->
         <section class="contact__form__section">
-          <label for="name">Name</label>
-          <input type="text" name="name"/>
+          <section class="contact__form__section__label">
+            <label for="name">Name</label>
+            <logo-icon/>
+          </section>
+          <input type="text" name="name" v-model="name" required/>
         </section>
         <section class="contact__form__section">
+          <section class="contact__form__section__label">
           <label for="email">Email</label>
-          <input type="text" name="email"/>
+            <logo-icon/>
+          </section>          
+          <input type="email" name="email" v-model="email" required/>
         </section>
         <section class="contact__form__section">
-          <label for="subject">Subject</label>
-          <input type="text" name="subject"/>
+          <section class="contact__form__section__label">
+            <label for="subject">Subject</label>
+            <logo-icon/>
+          </section>          
+          <input type="text" name="subject" v-model="subject"/>
         </section>
         <div class="contact__form__section">
-          <label for="message">Message</label>
-          <textarea name="message"/>
+          <section class="contact__form__section__label">
+            <label for="message">Message</label>
+            <logo-icon/>
+          </section>          
+          <textarea name="message" v-model="message" required/>
         </div>
         <div class="contact__form__send">
           <primary-color-round-button>Send</primary-color-round-button>
         </div>
-      </div>
+      </form>
     </div>
   </base-section>
 
@@ -30,11 +42,35 @@
 <script>
 import BaseSection from 'Components/BaseSection';
 import PrimaryColorRoundButton from 'Components/PrimaryColorRoundButton';
+import LogoIcon from 'Icons/LogoIcon'; //@TODO replace with a checkmark icon
 
 
 export default {
   name: 'TheContactSection',
-  components: {BaseSection, PrimaryColorRoundButton},
+  data() {
+    return {
+      name: '',
+      email: '',
+      subject: '',
+      message: '',
+    }
+  },
+  components: {BaseSection, PrimaryColorRoundButton, LogoIcon},
+  methods: {
+    onSubmit() {
+      let message = {
+          name: this.name,
+          email: this.email,
+          subject: this.subject,
+          message: this.message,
+      };
+
+      if (message.subject === '') {
+        message.subject = 'Message sent from julianeasterly.com'
+      }
+      //@TODO send to a php script
+    },
+  },
 
 }
 </script>
@@ -100,16 +136,27 @@ export default {
       @include global.border-box;
 
       &__form {
-
+        // display: flex;
+        // flex-direction: column;
+        //justify-content: space-around;
         &__section {
           margin: 25px 0;
-          label {
-            display: block;
-            @include global.p-font($weight: 700);
-            color: global.$primary-black;
+
+          &__label {
+            display: flex;
+            align-items: center;
+            label {
+              @include global.p-font($weight: 700);
+              color: global.$primary-black;
+              margin-right: 5px;
+            }
+            svg {
+              fill: rgba(global.$primary-black, 0.25);
+              height: 15px;
+            }
           }
   
-          input[type='text'], textarea {
+          input, textarea {
             //resize: none;
             width: 100%;
             max-width: 100%;
@@ -131,7 +178,7 @@ export default {
 
           }
 
-          input[type='text'] {
+          input {
             height: 30px;
           }
 
