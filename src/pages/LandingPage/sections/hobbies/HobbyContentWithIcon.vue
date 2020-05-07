546404ce291f43f60f09635a259550fda8999311
Julian Easterly <julianeasterly@gmail.com>
2020-05-07 12:22:13 +0200

    changed v-slot to #

--- src/pages/LandingPage/sections/hobbies/HobbyContentWithIcon.vue
+++ src/pages/LandingPage/sections/hobbies/HobbyContentWithIcon.vue
@@ -1,39 +1,45 @@
 <template>
   <base-content-with-icon>
-    <template v-slot:icon><slot name="icon"/></template>
-    <template v-slot:title><slot name="title"/></template>
-    <template v-slot:description><slot name="description"/></template>
+    <template #icon>
+      <slot name="icon" />
+    </template>
+    <template #title>
+      <slot name="title" />
+    </template>
+    <template #description>
+      <slot name="description" />
+    </template>
   </base-content-with-icon>
 </template>
 
 <script>
-import BaseContentWithIcon from 'Bases/BaseContentWithIcon';
+import BaseContentWithIcon from "Bases/BaseContentWithIcon";
 
 export default {
-  name: 'HobbyContentWithIcon',
-  components: {BaseContentWithIcon,}
-}
+  name: "HobbyContentWithIcon",
+  components: { BaseContentWithIcon }
+};
 </script>
 
 
 <style lang="scss" scoped>
-  @use 'global';
+@use 'global';
 
-  ::v-deep .base-content-with-icon {
-    
-    &__icon {
-      svg, img {
-        fill: global.$primary-color;
-      }
-    }
-
-    &__title {
-      @include global.h4-font($weight: 300);
-      color: global.$primary-black;
-    }
-    
-    &__description {
-      display: none;
+::v-deep .base-content-with-icon {
+  &__icon {
+    svg,
+    img {
+      fill: global.$primary-color;
     }
   }
+
+  &__title {
+    @include global.h4-font($weight: 300);
+    color: global.$primary-black;
+  }
+
+  &__description {
+    display: none;
+  }
+}
 </style>
--- src/pages/TheLandingPage/sections/TheEducationSection/EducationExperienceItem.vue
+++ src/pages/TheLandingPage/sections/TheEducationSection/EducationExperienceItem.vue
@@ -1,58 +1,69 @@
 <template>
   <div>
     <base-experience>
-      <template v-slot:img><slot name="img"/></template>
-      <template v-slot:title><slot name="title"/></template>
-      <template v-slot:subtitle><slot name="rank"/></template>
-      <template v-slot:subtitle-2><slot name="school-name"/></template>
-      <template v-slot:dates><slot name="dates"/></template>
-      <template v-slot:location><slot name="location"/></template>
-      <template v-slot:details></template>
+      <template #img>
+        <slot name="img" />
+      </template>
+      <template #title>
+        <slot name="title" />
+      </template>
+      <template #subtitle>
+        <slot name="rank" />
+      </template>
+      <template #subtitle-2>
+        <slot name="school-name" />
+      </template>
+      <template #dates>
+        <slot name="dates" />
+      </template>
+      <template #location>
+        <slot name="location" />
+      </template>
+      <template #details></template>
     </base-experience>
   </div>
 </template>
 
 <script>
-import BaseExperience from 'Bases/BaseExperience';
+import BaseExperience from "Bases/BaseExperience";
 
 export default {
-  name: 'EducationExperienceItem',
-  components: {BaseExperience,},
-}
+  name: "EducationExperienceItem",
+  components: { BaseExperience }
+};
 </script>
 
 <style lang="scss" scoped>
-  @use 'global';
-  
-  ::v-deep .base-exp {
-    display: flex;
-    margin: 60px 0;
-    
-    &__vital-info {
+@use 'global';
 
-      &__text {
-        &__title {
-          color: global.$primary-black;
-        }
+::v-deep .base-exp {
+  display: flex;
+  margin: 60px 0;
 
-        &__subtitle {
-          color: global.$primary-color;
-        }
+  &__vital-info {
+    &__text {
+      &__title {
+        color: global.$primary-black;
+      }
 
-        &__subtitle-2 {
-          color: global.$primary-black;
-        }
-        &__dates {
-          color: global.$primary-black;
-        }
+      &__subtitle {
+        color: global.$primary-color;
+      }
 
-        &__location {
-          color: global.$primary-black;
-          svg {
-            fill: global.$primary-color;
-          }
+      &__subtitle-2 {
+        color: global.$primary-black;
+      }
+      &__dates {
+        color: global.$primary-black;
+      }
+
+      &__location {
+        color: global.$primary-black;
+        svg {
+          fill: global.$primary-color;
         }
       }
     }
-  } 
+  }
+}
 </style>
