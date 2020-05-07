8a2cc061d680c4d47ac5f5b26ca478a6752ce92a
Julian Easterly <julianeasterly@gmail.com>
2020-05-07 12:22:13 +0200

    changed v-slot to #

--- src/pages/LandingPage/sections/hobbies/index.vue
+++ src/pages/LandingPage/sections/hobbies/index.vue
@@ -2,62 +2,69 @@
   <base-section id="hobbies" title="A Few of My Hobbies">
     <div class="hobbies">
       <hobby-content-with-icon class="hobbies__hobby">
-          <template v-slot:icon><logo-icon/></template>
-          <template v-slot:title>Coding and Design</template>
+        <template #icon>
+          <logo-icon />
+        </template>
+        <template #title>Coding and Design</template>
       </hobby-content-with-icon>
       <hobby-content-with-icon class="hobbies__hobby">
-          <template v-slot:icon><logo-icon/></template>
-          <template v-slot:title>Sports</template>
+        <template #icon>
+          <logo-icon />
+        </template>
+        <template #title>Sports</template>
       </hobby-content-with-icon>
       <hobby-content-with-icon class="hobbies__hobby">
-          <template v-slot:icon><logo-icon/></template>
-          <template v-slot:title>Do-It-Yourself</template>
+        <template #icon>
+          <logo-icon />
+        </template>
+        <template #title>Do-It-Yourself</template>
       </hobby-content-with-icon>
       <hobby-content-with-icon class="hobbies__hobby">
-          <template v-slot:icon><logo-icon/></template>
-          <template v-slot:title>Gardening</template>
+        <template #icon>
+          <logo-icon />
+        </template>
+        <template #title>Gardening</template>
       </hobby-content-with-icon>
       <hobby-content-with-icon class="hobbies__hobby">
-          <template v-slot:icon><logo-icon/></template>
-          <template v-slot:title>Cooking</template>
+        <template #icon>
+          <logo-icon />
+        </template>
+        <template #title>Cooking</template>
       </hobby-content-with-icon>
     </div>
   </base-section>
-
 </template>
 
 <script>
-import BaseSection from 'Bases/BaseSection';
-import HobbyContentWithIcon from './HobbyContentWithIcon';
-import LogoIcon from 'Icons/LogoIcon';
+import BaseSection from "Bases/BaseSection";
+import HobbyContentWithIcon from "./HobbyContentWithIcon";
+import LogoIcon from "Icons/LogoIcon";
 
 export default {
-  name: 'TheHobbiesSection',
-  components: {BaseSection, HobbyContentWithIcon, LogoIcon,},
-
-}
+  name: "TheHobbiesSection",
+  components: { BaseSection, HobbyContentWithIcon, LogoIcon }
+};
 </script>
 
 <style lang="scss" scoped>
-  @use 'global';
-  
-  #hobbies {
-    padding-top: 50px;
-    background-color: global.$primary-white;
-    ::v-deep .section__title {
-      color: global.$primary-color;
-      @include global.h4-font;
-      text-transform: uppercase;
-      &:before {
-        content: '';
-      }
-    }
+@use 'global';
 
-    .hobbies {
-      &__hobby{
-        margin-top: 60px;
-      }
-    
+#hobbies {
+  padding-top: 50px;
+  background-color: global.$primary-white;
+  ::v-deep .section__title {
+    color: global.$primary-color;
+    @include global.h4-font;
+    text-transform: uppercase;
+    &:before {
+      content: "";
     }
   }
+
+  .hobbies {
+    &__hobby {
+      margin-top: 60px;
+    }
+  }
+}
 </style>
--- src/pages/TheLandingPage/sections/TheExperienceSection/ProfessionalExperienceItem.vue
+++ src/pages/TheLandingPage/sections/TheExperienceSection/ProfessionalExperienceItem.vue
@@ -1,57 +1,66 @@
 <template>
   <div>
     <base-experience>
-      <template v-slot:img><slot name="img"/></template>
-      <template v-slot:title><slot name="job-title"/></template>
-      <template v-slot:subtitle><slot name="company-name"/></template>
-      <template v-slot:dates><slot name="work-dates"/></template>
-      <template v-slot:location><slot name="location"/></template>
-      <template v-slot:details></template>
+      <template #img>
+        <slot name="img" />
+      </template>
+      <template #title>
+        <slot name="job-title" />
+      </template>
+      <template #subtitle>
+        <slot name="company-name" />
+      </template>
+      <template #dates>
+        <slot name="work-dates" />
+      </template>
+      <template #location>
+        <slot name="location" />
+      </template>
+      <template #details></template>
     </base-experience>
   </div>
 </template>
 
 <script>
-import BaseExperience from 'Bases/BaseExperience';
+import BaseExperience from "Bases/BaseExperience";
 
 export default {
-  name: 'ProfessionalExperienceItem',
-  components: {BaseExperience,},
-}
+  name: "ProfessionalExperienceItem",
+  components: { BaseExperience }
+};
 </script>
 
 <style lang="scss" scoped>
-  @use 'global';
-  
-  ::v-deep .base-exp {
-    display: flex;
-    margin: 60px 0;
-    
-    &__vital-info {
-      
-      &__text {
-        &__title {
-          color: global.$primary-white;
-        }
+@use 'global';
 
-        &__subtitle {
-          color: global.$primary-color;
-        }
+::v-deep .base-exp {
+  display: flex;
+  margin: 60px 0;
 
-        &__subtitle-2 {
-          color: global.$primary-white;
-        }
-        &__dates {
-          color: global.$primary-white;
-        }
+  &__vital-info {
+    &__text {
+      &__title {
+        color: global.$primary-white;
+      }
 
-        &__location {
-          color: global.$primary-color;
-          svg {
-            fill: global.$primary-color;
-          }
+      &__subtitle {
+        color: global.$primary-color;
+      }
+
+      &__subtitle-2 {
+        color: global.$primary-white;
+      }
+      &__dates {
+        color: global.$primary-white;
+      }
+
+      &__location {
+        color: global.$primary-color;
+        svg {
+          fill: global.$primary-color;
         }
       }
     }
-  } 
+  }
+}
 </style>
