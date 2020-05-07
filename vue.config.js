2e527eead5b53dfe0c8b23a179a6e6bd5a0de406
Julian Easterly <julianeasterly@gmail.com>
2020-05-07 09:11:35 +0200

    reedited folder organisation

--- vue.config.js
+++ vue.config.js
@@ -10,11 +10,13 @@ module.exports = {
     },
     chainWebpack: config => {
         let alias = config.resolve.alias;
+        alias.set('API', path.resolve(__dirname, 'src/api'));
         alias.set('Assets', path.resolve(__dirname, 'src/assets'));
         alias.set('Bases', path.resolve(__dirname, 'src/components/bases'));
         alias.set('Common', path.resolve(__dirname, 'src/components/common'));
         alias.set('Components', path.resolve(__dirname, 'src/components'));
         alias.set('Icons', path.resolve(__dirname, 'src/components/icons'));
+        alias.set('Lang', path.resolve(__dirname, 'src/lang'));
         alias.set('Layout', path.resolve(__dirname, 'src/components/layout'));
         alias.set('Pages', path.resolve(__dirname, 'src/pages'));
         alias.set('Source', path.resolve(__dirname,'src'));
