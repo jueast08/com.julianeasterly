4576f6aab75b46990c1f82774be48db2ba6626b2
Julian Easterly <julianeasterly@gmail.com>
2020-05-11 11:29:41 +0200

    Updated ScrollIntoViewObserver to use queryselectors instead of class names and a BEM modifier

--- src/utility/ScrollIntoViewObserver.js
+++ src/utility/ScrollIntoViewObserver.js
@@ -3,17 +3,17 @@
  */
 export default class ScrollIntoViewObserver {
   /**
-   * @param {Array} classesToModify : Array of classes (strings) that need to be modified
-   * @param {String} classBEMModifier: String representing the BEM modifier that will be added to each classToModify item. Defaults to --in-view
+   * @param {Array} querySelectorResults : an array of DOM elements. you should be passing document.querySelector('[query]')
+   * @param {String} _classToAdd: String representing the animation class to add
    * @param {Object} intersectionObserverOptions: Objet representing options as described here : https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
    */
   constructor(
-    classesToModify,
-    classBEMModifier = "--in-view",
+    querySelectorResults,
+    modifierClass,
     intersectionObserverOptions = {}
   ) {
-    this._classesToModify = classesToModify;
-    this._classModifier = classBEMModifier;
+    this._querySelectorResults = querySelectorResults;
+    this._modifierClass = modifierClass;
     this._intersectionObserverOptions = intersectionObserverOptions;
     this._observer = null;
     this._actionArray = [];
@@ -23,15 +23,15 @@ export default class ScrollIntoViewObserver {
 
   /**
    *
-   * @param {Array} querySelector : an array of DOM elements. you should be passing document.querySelector('[query]')
+   * @param {Array} querySelectorResults : an array of DOM elements. you should be passing document.querySelector('[query]')
    * @param {Boolean} addOnInView : determines whether the class modifier should be added when the querySelector comes into view
    * @param {*} removeOnOutOfView : determines whether the class modifier should be removed when the querySelector leaves view
    */
-  observe(querySelector, addOnInView = true, removeOnOutOfView = true) {
+  observe(querySelectorResults, addOnInView = true, removeOnOutOfView = true) {
     if (this._observer === null) {
       throw "Observer has not been initiated";
     }
-    this._observer.observe(querySelector);
+    this._observer.observe(querySelectorResults);
     this._actionArray.push({
       addOnInView: addOnInView,
       removeOnOutOfView: removeOnOutOfView,
@@ -59,6 +59,10 @@ export default class ScrollIntoViewObserver {
   }
 
   _handleOnScroll(entry, index) {
+    console.log(entry);
+    console.log(this._querySelectorResults);
+    console.log(this._modifierClass);
+
     if (this._triggerEvent(entry) && this._actionArray[index].addOnInView) {
       this._addAnimationClasses();
     } else if (this._actionArray[index].removeOnOutOfView) {
@@ -75,23 +79,23 @@ export default class ScrollIntoViewObserver {
   }
 
   _addAnimationClasses() {
-    this._classesToModify.forEach((className) => {
-      document
-        .getElementsByClassName(className)
-        .forEach((element) =>
-          element.classList.add(className + this._classModifier)
-        );
-    });
+    if (typeof this._querySelectorResults === Array) {
+      this._querySelectorResults.forEach((element) => {
+        element.classList.add(this._modifierClass);
+      });
+    } else {
+      this._querySelectorResults.classList.add(this._modifierClass);
+    }
   }
 
   _removeAnimationClasses() {
-    this._classesToModify.forEach((className) => {
-      document
-        .getElementsByClassName(className)
-        .forEach((element) =>
-          element.classList.remove(className + this._classModifier)
-        );
-    });
+    if (typeof this._querySelectorResults === Array) {
+      this._querySelectorResults.forEach((element) => {
+        element.classList.remove(this._modifierClass);
+      });
+    } else {
+      this._querySelectorResults.classList.remove(this._modifierClass);
+    }
   }
 
   get isIntersectionObserverAvailable() {
