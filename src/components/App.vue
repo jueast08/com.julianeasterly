a37d4816911236d53efe32718e6c047a60bf80ff
Julian Easterly <julianeasterly@gmail.com>
2020-05-20 15:17:36 +0200

    created loader overlay to help with page loading

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,27 +1,19 @@
 <template>
   <div id="app">
-    <div v-if="!loaded && !timeup" class="loader col-s-12">
-      <div class="loader__wrapper col-s-2">
-        <logo-icon />
-        <div class="loader__wrapper__message">Loading</div>
-      </div>
-    </div>
-    <!-- <the-under-construction-Page ref="constrution_page" v-if="!loaded" /> -->
-    <the-Landing-Page ref="landing-page" v-else />
+    <loader-overlay :animation-delay="'5s'" v-if="!loaded || !timeup" />
+    <the-landing-page ref="landing-page" v-else />
   </div>
 </template>
 
 <script>
 import TheLandingPage from "Pages/TheLandingPage";
-// import TheUnderConstructionPage from "Pages/TheUnderConstructionPage";
-import LogoIcon from "UI/LogoIcon";
+import LoaderOverlay from "UI/LoaderOverlay";
 
 export default {
   name: "App",
   components: {
     TheLandingPage,
-    // TheUnderConstructionPage,
-    LogoIcon
+    LoaderOverlay
   },
   data() {
     return {
@@ -35,109 +27,20 @@ export default {
     }
   },
   async mounted() {
-    window.addEventListener("load", () => {
+    const closeLoader = () => {
+      if (process.env.NODE_ENV === "development") {
+        console.log(
+          "fully loaded",
+          this.timeup ? "" : "and awaiting animation timer"
+        );
+      }
       this.loaded = true;
-      window.removeEventListener("load");
-    });
-
-    await new Promise(r => setTimeout(r, 4000)).then(
+      window.removeEventListener("load", closeLoader);
+    };
+    window.addEventListener("load", closeLoader);
+    await new Promise(r => setTimeout(r, 2000)).then(
       () => (this.timeup = true)
     );
-    window.addEventListener("load", () => {
-      this.loaded = true;
-      window.removeEventListener("load");
-    });
   }
 };
 </script>
-
-<style lang="scss" scoped>
-@use 'global';
-
-.hide {
-  display: hidden;
-}
-
-.loader {
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  background: global.$primary-black;
-  &__wrapper {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    ::v-deep .logo-icon {
-      width: 25%;
-      &__secondary {
-        fill: global.$primary-white;
-      }
-      &__main {
-        fill: global.$primary-white;
-        &__arrow {
-          transform-origin: center;
-          animation: arrow_rotate 3s infinite;
-          @include global.keyframes(arrow_rotate) {
-            0% {
-              transform: rotate(-55deg);
-              fill: global.$primary-white;
-            }
-            25% {
-              transform: rotate(-60deg);
-              fill: global.$primary-white;
-            }
-            50% {
-              transform: rotate(-50deg);
-              fill: global.$primary-white;
-            }
-            75% {
-              transform: rotate(-55deg);
-              fill: global.$primary-white;
-            }
-            100% {
-              transform: rotate(0deg);
-              fill: global.$primary-color;
-            }
-          }
-        }
-        &__direction {
-          transform-origin: center;
-          animation: to-green 2s infinite;
-          @include global.keyframes(to-green) {
-            0% {
-            }
-            25% {
-            }
-            50% {
-              fill: global.$primary-white;
-            }
-            75% {
-              fill: global.$primary-white;
-            }
-            100% {
-              fill: global.$primary-color;
-            }
-          }
-        }
-      }
-    }
-    &__message {
-      margin-top: 20px;
-      color: global.$primary-white;
-      @include global.p-font;
-      animation: pulse 2s infinite alternate;
-      @include global.keyframes(pulse) {
-        0% {
-          opacity: 0;
-        }
-        100% {
-          opacity: 1;
-        }
-      }
-    }
-  }
-}
-</style>
